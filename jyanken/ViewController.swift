f2aeab06180425d13b2d7723c9373ade2eb1693c
IwaoTakafumi <koshiiwao2904@icloud.com>
2018-12-05 13:37:15 +0900

    jyanken

--- jyanken/ViewController.swift
+++ jyanken/ViewController.swift
@@ -9,10 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var mybutton: UIButton!
+  
+  @IBOutlet weak var imageview: UIImageView!
+  
+  @IBOutlet weak var label: UILabel!
+  
+  //押すとジャンケン発動
+  @IBAction func button(_ sender: UIButton) {
+    //ランダムに変える
+    let random = arc4random_uniform(3)
+    print(random)
+    
+    //0の場合グー
+    if random == 0 {
+     imageview.image = UIImage(named: "gu")
+      //ラベルも変える
+     label.text = "グー"
+    } else if random == 1 {
+      //1の場合チョキ
+      imageview.image = UIImage(named: "choki")
+      label.text = "チョキ"
+    } else if random == 2 {
+      //2の場合
+      imageview.image = UIImage(named: "pa")
+      label.text = "パー"
+    }
+    
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    //背景色を変える
+    self.view.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.9137254902, blue: 0.5843137255, alpha: 1)
+    //imageviewを自動的にサイズを合わせる
+    imageview.contentMode = UIView.ContentMode.scaleAspectFit
+//    self.view.addSubview(imageview)
+    
   }
 
 
