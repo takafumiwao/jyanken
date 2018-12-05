95a4235a5a9e28e92af73d408771cc633cd7ecea
IwaoTakafumi <koshiiwao2904@icloud.com>
2018-12-05 12:40:09 +0900

    Initial Commit

---
+++ jyanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jyanken
+//
+//  Created by 岩男高史 on 2018/12/05.
+//  Copyright © 2018 岩男高史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
